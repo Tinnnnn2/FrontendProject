3d8c9a6f473d2cc7ff60bcf4f57675a7469465ca
pigtun <minitinvv@gmail.com>
2024-03-02 01:18:25 +0700

    1.0

---
+++ index.js
@@ -0,0 +1,33 @@
+    const express = require('express');
+    const axios = require('axios');
+    const path = require("path");
+    const app = express();
+    var bodyParser = require('body-parser');
+
+    const base_url = "http://localhost:3000";
+
+    app.set("views",path.join(__dirname,"/public/views"));
+    app.set('view engine','ejs');
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: false}));
+
+    app.use(express.static(__dirname + '/public'));
+
+    app.get("/",async(req, res) => {
+            const response = await axios.get(base_url + '/Products');
+            res.render("shop", {Products : response.data});
+    });
+
+    app.get("/login",async(req, res) => {
+        const response = await axios.get(base_url + '/users');
+        res.render("login", {users : response.data});
+    });
+
+    app.get("/Register",async(req, res) => {
+        const response = await axios.get(base_url + '/users');
+        res.render("Reigister", {users : response.data});
+    });
+
+    app.listen(5500, () => {
+        console.log('Sever started on post 5500');
+    });
