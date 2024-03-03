6d9ea3b7ebb7b48b07724eba21818ed22c4e2b01
pigtun <minitinvv@gmail.com>
2024-03-03 18:13:34 +0700

    save

--- index.js
+++ index.js
@@ -1,62 +1,116 @@
-    const express = require('express');
-    const axios = require('axios');
-    const path = require("path");
-    const app = express();
-    var bodyParser = require('body-parser');
+const express = require('express');
+const axios = require('axios');
+const path = require("path");
+const app = express();
+var bodyParser = require('body-parser');
+const multer  = require('multer');
 
-    const base_url = "http://localhost:3000";
+const base_url = "http://localhost:3000";
 
-    app.set("views",path.join(__dirname,"/public/views"));
-    app.set('view engine','ejs');
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({ extended: false}));
+app.set("views",path.join(__dirname,"/public/views"));
+app.set('view engine','ejs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
 
-    app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
-    app.get("/",async(req, res) => {
-            const response = await axios.get(base_url + '/Products');
-            res.render("shop", {Products : response.data});
-    });
+// auto img 
+// const putguitar = multer.diskStorage({
+// destination: function (req, file, cb) {
+//     cb(null, path.join(__dirname, './public/pictures/Guitar'));
+// },
+// filename: function (req, file, cb) {
+//     cb(null, file.originalname);
+// }
+// });
 
-    app.get("/adidas",async(req, res) => {
-        const response = await axios.get(base_url + '/adidas');
-        res.render("adidas", {adidas : response.data});
-    });
+// const guitar = multer({ storage: putguitar });
 
-    app.get("/nike",async(req, res) => {
-        const response = await axios.get(base_url + '/nikes');
-        res.render("nike", {nikes : response.data});
-    });
-
-    app.get("/converse",async(req, res) => {
-        const response = await axios.get(base_url + '/converses');
-        res.render("converse", {converses : response.data});
-    });
-
-    app.get("/login",async(req, res) => {
-        const response = await axios.get(base_url + '/users');
-        res.render("login", {users : response.data});
-    });
-
-    app.get("/Register",async(req, res) => {
-        res.render("Register");
-    });
-
-    app.post("/Register",async(req, res) => {
-        const data = {
-            username:req.body.username, 
-            email:req.body.email,              
-            password:req.body.password,     
-            phone:req.body.phone,         
-            userAdress:req.body.userAdress 
-        }
-        await axios.post(base_url + '/users',data)
-        return res.redirect("/");   
-    });
+app.get("/",async(req, res) => {
+    const response = await axios.get(base_url + "/Products");
+    const response2 = await axios.get(base_url + "/Types");
+    res.render("shop", {product: response.data, type: response2.data});
+});
 
 
+app.get("/nike/:id",async (req, res) => {
+    const response = await axios.get(base_url + "/Products");
+    const product = response.data;
+
+    let dt = [];
+    for (let pro of product) {
+        if (pro.Typeid == req.params.id) {
+            const response = await axios.get(base_url + "/Products/" + pro.Productid);
+            const data = response.data;
+            dt.push(data);
+        }   
+    }
+
+    const response2 = await axios.get(base_url + "/Types");
+    res.render("nike", {dt: dt, type: response2.data});
+});
+//-----------------------------------------------
+app.get("/adidas/:id",async (req, res) => {
+    const response = await axios.get(base_url + "/Products");
+    const product = response.data;
+
+    let ad = [];
+    for (let pro of product) {
+        if (pro.Typeid == req.params.id) {
+            const response = await axios.get(base_url + "/Products/" + pro.Productid);
+            const data = response.data;
+            ad.push(data);
+        }   
+    }
+
+    const response2 = await axios.get(base_url + "/Types");
+    res.render("adidas", {ad: ad, type: response2.data});
+});
+//----------------------------------------------------------------------
+app.get("/converse/:id",async (req, res) => {
+    const response = await axios.get(base_url + "/Products");
+    const product = response.data;
+
+    let cv = [];
+    for (let pro of product) {
+        if (pro.Typeid == req.params.id) {
+            const response = await axios.get(base_url + "/Products/" + pro.Productid);
+            const data = response.data;
+            cv.push(data);
+        }   
+    }
+
+    const response2 = await axios.get(base_url + "/Types");
+    res.render("converse", {cv: cv, type: response2.data});
+});
 
 
-    app.listen(5500, () => {
-        console.log('Sever started on post 5500');
-    });
+
+
+app.get("/login",async(req, res) => {
+    const response = await axios.get(base_url + '/users');
+    res.render("login", {users : response.data});
+});
+
+app.get("/Register",async(req, res) => {
+    res.render("Register");
+});
+
+app.post("/Register",async(req, res) => {
+    const data = {
+        username:req.body.username, 
+        email:req.body.email,              
+        password:req.body.password,     
+        phone:req.body.phone,         
+        userAdress:req.body.userAdress 
+    }
+    await axios.post(base_url + '/users',data)
+    return res.redirect("/");   
+});
+
+
+
+
+app.listen(5500, () => {
+    console.log('Sever started on post 5500');
+});
